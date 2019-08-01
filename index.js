19e030dcf0c7601046422a4825ea8d3dacd75429
Adam <balassaadi@gmail.com>
2019-08-01 13:15:04 +0200

    indexjs

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.set('trust proxy', 1)
+
+app.use(express.static('assets'))
+app.set('view engine', 'ejs')
+app.set('views', 'view')
+
+app.get('/', (req, res) => {
+    res.send('Mizu');
+})
+
+app.use(function (req, res) {
+  res.redirect('/404')
+})
+
+
+app.listen(3000, 'localhost', () => {
+    console.log('Server started on port 3000');
+});
+
+module.exports = app;
