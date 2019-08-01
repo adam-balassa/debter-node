e705fc2bb3506f64ad9f1a37dc1b805eb740860a
Adam <balassaadi@gmail.com>
2019-08-01 15:14:19 +0200

    tslint fixed

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-const app = express()
+const app = express();
 
-app.set('port', (process.env.PORT || 3000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(app.get('port'), function () {
+  console.log('Node app is running at localhost:' + app.get('port'));
+});
