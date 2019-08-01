3b0d5734cb0ce43f9be5dab225d79d092c243921
Adam <balassaadi@gmail.com>
2019-08-01 15:56:34 +0200

    mysql connection set

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import { ConnectionConfig, Connection, createConnection } from 'mysql';
 const app = express();
+const mysqlConfig: ConnectionConfig = {
+  host: 'remotemysql.com',
+  database: 'BkMfl7pWj2',
+  user: 'BkMfl7pWj2',
+  password: 'xlzJtt2zTl',
+  port: 3306
+};
 
 app.set('port', (process.env.PORT || 3000));
 app.use(function(req, res, next) {
@@ -13,7 +21,12 @@ app.get('/', function (req, res) {
 });
 
 app.get('/test', function (req, res) {
-  res.send({error: false});
+  const connection: Connection = createConnection(mysqlConfig);
+  connection.connect();
+  connection.query('select * from test_table', (error, result) => {
+    res.send(error ? error : result);
+  });
+  connection.end();
 });
 
 app.listen(app.get('port'), function () {
