d560b45b0a940e2caa6061904123df65ba543aab
Adam <balassaadi@gmail.com>
2019-08-01 15:25:46 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import express from 'express';
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
