92af0c49bca52b86f8cedbf80d22d6b2e60323ee
Adam <balassaadi@gmail.com>
2019-08-23 23:37:57 +0200

    hotfix

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const router_1 = require("./router/router");
 const app = express_1.default();
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
+const router = express_1.default.Router();
 app.set('port', (process.env.PORT || 3000));
-app.use(express_1.default.static(__dirname + '/public'));
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
+const routes = new router_1.Routes(router);
+app.use(router);
 app.listen(app.get('port'), function () {
-    console.log("Node app is running at localhost:" + app.get('port'));
+    console.log('Node app is running at localhost:' + app.get('port'));
 });
--- src/router/router.ts
+++ src/router/router.ts
@@ -62,11 +62,5 @@ export class Routes {
         .then(result => { res.send(result); })
         .catch(error => { res.send(error); });
     });
-
-    this.router.post('/import', (req: Request, res: Response) => {
-      this.controller.refreshDebt(req.body.roomKey)
-        .then(result => { res.send(result); })
-        .catch(error => { res.send(error); });
-    });
   }
 }
